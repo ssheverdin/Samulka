8a5215df56e3f50323a36701d812d8d87988dd66
vvsheverdin <63597428+vvsheverdin@users.noreply.github.com>
2020-08-06 08:51:06 -0500

    dataunit

--- Package/DataUnitOfWork/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DataUnitOfWork
-{
-    public class Class1
-    {
-    }
-}
---
+++ Package/DataUnitOfWork/DataUnitOfWork/SqlServerDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Reflection;
+using DataUnitOfWork.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataUnitOfWork
+{
+    public abstract class SqlServerDbContext : DbContext
+    {
+        protected SqlServerDbContext(DbContextOptions<SqlServerDbContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var baseType = typeof(EntityBase);
+            var entityTypes = Assembly.GetExecutingAssembly().GetTypes().Where(i => i.IsClass && i != baseType && baseType.IsAssignableFrom(i)).ToList();
+            foreach (var type in entityTypes)
+            {
+                modelBuilder.Entity(type).HasKey("Id");
+            }
+            modelBuilder.EnableAutoHistory(null);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
